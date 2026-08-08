cbc970a127727f70e6afc3476194f5645b0b8faa
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-08-08 14:54:49 +0300

    fix app listen issue

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
 }
 
 bootstrap();
